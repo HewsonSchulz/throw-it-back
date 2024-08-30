8a0487ff4d909d1bb462dd423096895c8f2cca15
Hewson Schulz <hewsonschulz@gmail.com>
2024-08-30 09:23:24 -0500

    pixel art shader setup/implementation

--- scripts/prologue_bg.gd
+++ scripts/prologue_bg.gd
@@ -1,10 +1,14 @@
 extends AnimatedSprite2D
 
 func _ready():
-	# set initial modulate to black
-	modulate = Color(0, 0, 0, 1)
-	
-	fade(5, 1)
+	# set initial fade to 0 (black)
+	set_fade(0.0)
+
+	fade(1, 2)
+
+func set_fade(value: float):
+	# modifies shader `fade` parameter
+	material.set_shader_parameter("fade", value)
 
 func fade(delay: float, duration: float):
 	var tween = create_tween()
@@ -12,5 +16,5 @@ func fade(delay: float, duration: float):
 	# wait
 	tween.tween_interval(delay)
 	
-	# tween modulate property from black to white
-	tween.tween_property(self, "modulate", Color(1, 1, 1, 1), duration)
+	# tween fade property from 0 to 1
+	tween.tween_method(set_fade, 0.0, 1.0, duration)
